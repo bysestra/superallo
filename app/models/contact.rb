e2aaec0b34cd7e911998cf19ff8492221bc4eb67
Adam Miribyan <adam@bysestra.com>
2020-04-27 20:37:25 +0200

    Basic contact validations.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,6 @@ class Contact < ApplicationRecord
   belongs_to :account
   has_many :calls, foreign_key: :callee_id, inverse_of: :callee
   has_many :callers, through: :calls, source: :creator
+
+  validates :first_name, :last_name, :phone_number, presence: true
 end
