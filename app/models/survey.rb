fd412a20fb7a778fedb290664b806d899bdd0d24
Adam Miribyan <adam@bysestra.com>
2020-05-07 22:59:52 +0200

    New contact perma layout with surveys.

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,6 +2,6 @@ class Survey < ApplicationRecord
   include Accountable
 
   def render(surveyee:)
-    Liquid::Template.parse(template).render(surveyee.attributes)
+    Liquid::Template.parse(template).render(surveyee.attributes).html_safe
   end
 end
