2f5246f34b8a0cfaedb7a4e28ba690c12494625e
Adam Miribyan <adam@bysestra.com>
2020-05-17 21:29:35 +0200

    Make sure the datepicker persists the changed date.

--- app/javascript/controllers/datetime_controller.js
+++ app/javascript/controllers/datetime_controller.js
@@ -1,11 +1,18 @@
-require('jquery-ui/themes/base/datepicker')
-require('jquery-ui/ui/widgets/datepicker')
+require("jquery-ui/themes/base/datepicker")
+require("jquery-ui/ui/widgets/datepicker")
 
 export default class extends ApplicationController {
-  static targets = [ 'date', 'time', 'destination' ]
+  static targets = [ "date", "time", "destination" ]
 
   connect() {
-    this.$date.datepicker({})
+    this.$date.datepicker({
+      dateFormat: "dd/mm/yy",
+      minDate: 0,
+      onSelect: (value, _) => {
+        this.destinationTarget.value = value
+        this.destinationTarget.dispatchEvent(new Event("change"))
+      }
+    })
   }
 
   get $date() {
